270e8c881495b81d8f1b38572b96b9461bd00f0a
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-06-22 15:26:09 +0300

    chore: added mlab

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-mongoose.connect(config.database);
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+var mongodbUri = 'mongodb://hackatonas:hackatonas12@ds117209.mlab.com:17209/matcher';
+
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;
+
+conn.once('open', function() {
+  console.log("Mongo connected");
+});
+
+conn.on('error', console.error.bind(console, 'connection error:'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
