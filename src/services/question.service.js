b966557e6be237bdc9e28b92b5037e77bfc874db
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-07-02 22:16:12 +0300

    feat: user can vote on question ref

--- src/services/question.service.js
+++ src/services/question.service.js
@@ -18,34 +18,44 @@ function vote(req, res) {
     return res.json({error: true, field: 'answer', message: 'Please provide your answer'});
   }
 
-  let answerModel = new AnswerModel({
-    answer: req.body.answer,
-    user: req.body.userEmail,
-    timestamp: new Date().getTime()
-  });
-
-  answerModel.save((err, answer) => {
-    if (err) {
-      return res.json({error: true, message: 'Failed to save your answer, please try again'});
-    }
-
-    QuestionModel.findById(req.body.questionId, (err, question) => {
+  QuestionModel
+    .findById(req.body.questionId)
+    .populate({path: 'answers', select: 'user'})
+    .exec((err, question) => {
       if (!question) {
         return res.json({error: true, message: 'There is no such question with given ID'});
       }
 
-      question.answers.push(answer._id);
+      let alreadyVoted = question.answers.find((answer) => {
+        return answer.user === req.body.userEmail;
+      });
 
-      question.save((err, question) => {
-        if (err) {
-          return res.json({error: true, message: 'Question cannot be updated for mysterious reasons'});
-        }
-        res.json({success: true, message: 'Vote added'});
-      })
+      if (alreadyVoted) {
+        return res.json({error: true, message: 'Already voted'});
+      } else {
+
+        let answerModel = new AnswerModel({
+          answer: req.body.answer,
+          user: req.body.userEmail,
+          timestamp: new Date().getTime()
+        });
+
+        answerModel.save((err, answer) => {
+          if (err) {
+            return res.json({error: true, message: 'Failed to save your answer, please try again'});
+          }
+
+          question.answers.push(answer._id);
+
+          question.save((err, question) => {
+            if (err) {
+              return res.json({error: true, message: 'Question cannot be updated for mysterious reasons'});
+            }
+            res.json({success: true, message: 'Vote added'});
+          })
+        });
+      }
     });
-  })
-
-
 }
 
 module.exports = PollService;
